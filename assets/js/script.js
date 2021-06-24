eaa53d88c5d402ac23a3f4dabdbc48f81841fda7
georghiton-luiz <georghiton@hotmail.com>
2021-06-24 12:55:16 -0300

    Recuperando dados pelo JavaScrip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-console.log('Olá Mundo');
+let email = 'joao@hcode.com.br';
+console.log(email);
+
+email = 'glaucio@hcode';
+console.log(email);
+console.log('O seu e-mail é: ' + email);
+console.log(`O seu e-mail é: ${email}`);
+
+
+document.getElementById('btn-submit').addEventListener('click', e =>{
+    console.log('O botão foi clicado');
+});
+
+document.getElementById('form-login').addEventListener('mouseenter', e =>{
+    console.log('O mouse está sobre o formulário');    
+});
+
+document.querySelector('#form-login').addEventListener('mouseleave', e => {
+    console.log('O mouse está fora do formulário');  
+});
+
+document.querySelector('#form-login').addEventListener('submit', e =>{
+    e.preventDefault();
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    console.log(email, password);
+});
